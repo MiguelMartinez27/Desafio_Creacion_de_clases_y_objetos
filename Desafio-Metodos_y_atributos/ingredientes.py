bccba4a31aa835a0ea4956c21b5449755c9f45ba
MiguelMartinez27 <miguel.martinez.fue@gmail.com>
2024-08-26 18:22:53 -0400

    docstring added

--- Desafio-Metodos_y_atributos/ingredientes.py
+++ Desafio-Metodos_y_atributos/ingredientes.py
@@ -1,8 +1,11 @@
 # Lista de ingredientes proteicos posibles
 proteicos = ["pollo", "vacuno", "carne vegetal"]
+"""Lista de ingredientes proteicos"""
 
 # Lista de ingredientes vegetales posibles
 vegetales = ["tomate", "aceitunas", "champiñones"]
+"""Lista de ingredientes vegetales"""
 
 # Lista de tipos de masa posibles
 masas = ["tradicional", "delgada"]
+"""lista de masas"""
